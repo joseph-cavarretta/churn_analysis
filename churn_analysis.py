17776b191127fa69466d8d0be3e27600151491e4
Joseph Cavarretta <57957983+joseph-cavarretta@users.noreply.github.com>
2021-06-03 09:55:58 -0600

    Update churn_analysis.py

--- churn_analysis.py
+++ churn_analysis.py
@@ -4,15 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 pd.options.display.float_format = '${:,.2f}'.format
 
-# Format Currency Function for Plotting
-def formatCurrency(x):
-    if x < 0:
-        return "$({:,.2f})".format(abs(x))
-    else:
-        return "${:,.2f}".format(x)
-
 ############### OPEN CSV & READ INTO DATAFRAME ###############
-file_name = 'Churn_analysis_East'
+file_name = 'Churn_analysis'
 final_file_name = file_name + '.csv'
 read_my_file = pd.read_csv(final_file_name)
 df = pd.DataFrame(read_my_file)
@@ -27,16 +20,16 @@ df.replace(to_replace =" $-   ", value ='0', inplace = True)                    
 df['Attributed Churn'] = df['Attributed Churn'].astype(float)
 
 ############### PLOT TOTAL CHURN BY GEO (BAR)) ###############
-geo_total = df.groupby(['Geography'])['Attributed Churn'].sum().sort_values()
-geo_total.plot.bar(title = 'Attributed Churn by GEO 2019-2020')
-plt.xlabel('Geography')
+geo_total = df.groupby(['Location'])['Attributed Churn'].sum().sort_values()
+geo_total.plot.bar(title = 'Attributed Churn by Location 2019-2020')
+plt.xlabel('Location')
 plt.xticks(rotation=45, ha='right')
 plt.ylabel("Attributed Churn Bookings")
 plt.show()
 
 ############### PLOT TOP 10 MARKETS BY CHURN (BAR) ###############
-market_total = df.groupby(['Market Lookup'])['Attributed Churn'].sum().sort_values().head(10)
-market_total.plot.bar(title = 'Attributed Churn by GEO 2019-2020')
+market_total = df.groupby(['Market'])['Attributed Churn'].sum().sort_values().head(10)
+market_total.plot.bar(title = 'Attributed Churn by Location 2019-2020')
 plt.xlabel('Market')
 plt.xticks(rotation=45, ha='right')
 plt.ylabel("Attributed Churn Bookings")
